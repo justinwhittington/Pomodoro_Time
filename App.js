ad4b5681ecc86d4c0f4138f85eed68f62cb7fae8
Justin <jwhittington1992@gmail.com>
2019-06-07 16:44:30 -0400

    first

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello There Ezra</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+// import {vibrate} from './utils'
+
+// // causes phone to vibrate
+// vibrate()
